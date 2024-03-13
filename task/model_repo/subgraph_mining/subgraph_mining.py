1091e44790d67011b5e6ef958afa2db1d7afad99
Li Zhang <zhanglizl80@gmail.com>
2024-03-14 03:04:20 +0800

    update

--- task/model_repo/subgraph_mining/subgraph_mining.py
+++ task/model_repo/subgraph_mining/subgraph_mining.py
@@ -21,12 +21,12 @@ def subgraph_mining(table, node_type, edge_type, node_id=0, directed = "undirect
                 for record in nodes:
                     node_id = record["id"]
                     print(f"node id : {node_id}")
-                    result = session.execute_write(mining_3_hop_subgraph_tx, node_id=node_id, edge_type=edge_type, directed=directed)
+                    result = session.execute_write(mining_3_hop_subgraph_tx, node_type=node_type, node_id=node_id, edge_type=edge_type, directed=directed)
 
             else:
                 nodes = session.execute_read(query_name_get_nodes_tx, node_type=node_type)
                 if node_id in [record["id"] for record in nodes]:
-                    result = session.execute_write(mining_3_hop_subgraph_tx, node_id=node_id, edge_type=edge_type, directed=directed)
+                    result = session.execute_write(mining_3_hop_subgraph_tx, node_type=node_type, node_id=node_id, edge_type=edge_type, directed=directed)
                 else:
                     result = "node not exist"
     
@@ -40,7 +40,7 @@ def query_name_get_nodes_tx(tx, node_type):
     )
     return list(target_id)
 
-def mining_3_hop_subgraph_tx(tx, node_id, edge_type, directed):
+def mining_3_hop_subgraph_tx(tx, node_type, node_id, edge_type, directed):
     # 获取指定节点的3跳子图节点ID
     # MATCH path = (start)-[*1..3]-(end) 表示从start出发3跳之内与end相连
     # MATCH path = (start)-[*1..3]->(end) 表示从start出发3跳之内到end节点（有向关系）
@@ -54,11 +54,13 @@ def mining_3_hop_subgraph_tx(tx, node_id, edge_type, directed):
     neighbor = tx.run(f"""
         MERGE (alertnodes:AlertNodes {{ alert_id: {node_id} }})
         WITH alertnodes
-        MATCH path = (start)-[*1..3]{edge_dir}(end)
-        WHERE start.id = {node_id} AND all(r IN relationships(path) WHERE TYPE(r) = '{edge_type}')
-        WITH collect(end) AS neighbor_list, end, alertnodes
+        MATCH path = (start:{node_type} {{ id: {node_id} }})-[*1..3]{edge_dir}(end:{node_type})
+        WHERE all(r IN relationships(path) WHERE TYPE(r) = '{edge_type}')
+        WITH collect(end) AS neighbor_list, start, end, alertnodes
         FOREACH (n IN neighbor_list | 
                 MERGE (alertnodes)-[r:alert_service_Edge]->(n))
-        RETURN collect(end.id) AS neighbor
+        WITH start, end, alertnodes
+        MERGE (alertnodes)-[r:alert_service_Edge]->(start)
+        RETURN start.id AS core, collect(end.id) AS neighbor
         """)
     return list(neighbor)
